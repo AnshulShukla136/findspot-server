49815066a92770e75125047910de8e3e4094cfc4
AnshulShukla136 <anshulshukla136@gmail.com>
2026-03-24 23:06:44 +0530

    changes

--- utils/email.js
+++ utils/email.js
@@ -8,7 +8,7 @@ const createTransporter = () => {
     host: 'smtp.gmail.com',
     port: 465,
     secure: true,
-    family: 4,        // ← ADD THIS — forces IPv4
+    family: 4,       // ← ADD THIS — forces IPv4
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASS,
