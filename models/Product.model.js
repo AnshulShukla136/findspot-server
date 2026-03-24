5a1b548ce4574d5d91dde5e0c5aa1afca067fcb4
AnshulShukla136 <anshulshukla136@gmail.com>
2026-03-24 17:51:50 +0530

    checking

--- models/Product.model.js
+++ models/Product.model.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import mongoose from 'mongoose'
 
 const platformDataSchema = new mongoose.Schema({
@@ -26,35 +25,4 @@ const productSchema = new mongoose.Schema({
 }, { timestamps: true })
 
 productSchema.index({ title: 'text', searchKeywords: 'text' })
-
-=======
-import mongoose from 'mongoose'
-
-const platformDataSchema = new mongoose.Schema({
-  platform: String,
-  price: Number,
-  mrp: Number,
-  discount: Number,
-  url: String,
-  inStock: Boolean,
-  delivery: String,
-  lastUpdated: { type: Date, default: Date.now },
-})
-
-const productSchema = new mongoose.Schema({
-  title: { type: String, required: true },
-  brand: String,
-  category: String,
-  images: [String],
-  description: String,
-  rating: Number,
-  reviews: Number,
-  platforms: [platformDataSchema],
-  specs: [{ label: String, value: String }],
-  searchKeywords: [String],
-}, { timestamps: true })
-
-productSchema.index({ title: 'text', searchKeywords: 'text' })
-
->>>>>>> e49221474f114a779c286c41770d1ac46fa7ad76
 export default mongoose.model('Product', productSchema)
