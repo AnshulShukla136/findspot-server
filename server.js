976ca1a7100319cef959554680a308fd703b8501
AnshulShukla136 <anshulshukla136@gmail.com>
2026-03-30 09:41:25 +0530

    updated /api/health

--- server.js
+++ server.js
@@ -77,9 +77,7 @@ app.use('/api/products', productRoutes)
 // Health check
 app.get('/api/health', (req, res) => {
   res.json({
-    status: 'OK',
-    message: 'findSpot API is running 🚀',
-    environment: process.env.NODE_ENV,
+    status: 'OK'
   })
 })
 
